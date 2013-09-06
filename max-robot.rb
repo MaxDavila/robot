dcfe01cf6a156f95cd430493bf5d2b62e08551c2
dbcapprentice <dbcapprentice@gmail.com>
2013-09-06 00:06:11 -0700

    add API calls

--- max-robot.rb
+++ max-robot.rb
@@ -2,14 +2,14 @@ require 'mechanize'
 require 'open-uri'
 require "pry"
 
+EXIT_WORDS = ["exit", "quit", "q", "asdf"]
+ROBOT_NAMES = ["Lucile", "RTD2", "Rupert", "Bush", "Gwen", "Bruno", "C3PO"]
+QUESTION_WORDS = ["why", "what", "?", "who", "how", "where"]
+HELP_WORDS = ["help", "help!", "this sucks", "fuck you", "man", "you suck"]
+
 class AI
-  @@exit_words = ["exit", "quit", "q", "asdf"]
-  @@robot_names = ["Lucile", "RTD2", "Rupert", "Bush", "Gwen", "Bruno", "C3PO"]
-  @@question_words = ["why", "what", "?", "who", "how", "where"]
-  @@help_words = ["help", "help!", "this sucks", "fuck you", "man", "you suck"]
-  
   def initialize
-    @name = @@robot_names[rand(@@robot_names.length)]
+    @name = ROBOT_NAMES[rand(ROBOT_NAMES.length)]
     @human_input = nil
     @answer = nil
   end
@@ -17,34 +17,41 @@ class AI
   def greet
     puts "Hi my name is #{@name} ask me something interesting"
   end
-  
+
   def converse
-    
     begin
     @human_input = gets.chomp
     parse_input
-
-
     end while stop? != true
   end
 
-
+  def show_train
+    system('sl')
+  end
 
   def stop?
-    @@exit_words.each do |word|
-      return true if @human_input == word
-    end
+    EXIT_WORDS.include?(@human_input)
   end
-  
 
   def parse_input
     if input_include_question?
-      puts 
-      puts "Choose a topic by number"
-      get_answer.each_with_index do |option, index|
-        puts "#{index + 1}.- #{option}"
+      puts
+      puts "you asked a question, good for you"
+      puts
+      if get_fact_answer
+        #show_train
+        get_fact_answer.each_with_index do |item, index|
+          puts "Searching for #{item}....." if index.zero?
+          puts item
+          puts
+        end
+      else
+        #show_train
+        puts "Choose a topic by number"
+        get_answer.each_with_index do |option, index|
+          puts "#{index + 1}.- #{option}"
+        end
       end
-      
     elsif input_include_help_word?
       display_help
     elsif include_option?
@@ -64,7 +71,7 @@ class AI
     puts parser_result.parser.css('p').inner_text
   end
   def input_include_help_word?
-    @@help_words.each do |word|
+    HELP_WORDS.each do |word|
       return true if @human_input.include?(word) && @human_input.length < 10
     end
     false
@@ -75,17 +82,22 @@ class AI
   end
 
   def input_include_question?
-    @@question_words.each do |word|
+    QUESTION_WORDS.each do |word|
       return true if @human_input.include?(word) && @human_input.length > 8
     end
     false
   end
 
   def get_answer
-    puts "you asked a question, good for you"
-    puts 
-    @answer = Knowledge.new(@human_input)
+
+    @answer = GoogleKnowledge.new(@human_input)
     @answer.show_results
+  end
+
+  def get_fact_answer
+    @answer = WolframKnowledge.new(@human_input)
+    @answer.show_results
+
   end
 
 
@@ -95,9 +107,32 @@ class AI
 end
 
 
+class WolframKnowledge
+  attr_reader :question, :xml_doc
+  def initialize(question)
+    @question = URI::encode(question)
+    @xml_doc = Nokogiri::XML(open("http://api.wolframalpha.com/v2/query?input=#{@question}&appid=WWE2HH-VQ2K68XL88"))
+
+  end
+
+  def show_results
+    @xml_doc.css("plaintext").map { |element| element.inner_text } if success?
+  end
+
+  def get_result_title
+    @xml_doc.css("pod").map { |title| element.get_attribute("title") }
+  end
+
+  def success?
+    @xml_doc.css("queryresult")[0].get_attribute("success") == "true" &&
+    @xml_doc.css("queryresult")[0].get_attribute("error") == "false"
+  end
+end
 
 
-class Knowledge
+
+
+class GoogleKnowledge
   attr_reader :links
   def initialize(question)
     @question = question
@@ -112,7 +147,7 @@ class Knowledge
     @links = @agent.page.links.find_all { |l| l.attributes.parent.name == 'h3' }
     @agent.page.parser.css('.r > a').map { |link| link.inner_text }
   end
-  
+
 end
 
 # know = Knowledge.new("what is the meaning of life?")
@@ -122,7 +157,7 @@ rtd2.greet
 rtd2.converse
 #=> What do you want to talk about?
 # => list a bunch of topics from Fark.com
-# user selects topic 
-# scrapes the selected website to gain knowledge 
+# user selects topic
+# scrapes the selected website to gain knowledge
 # user asks questions about the topic
 # AI responds with random paragraphs
